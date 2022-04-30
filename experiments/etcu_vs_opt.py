8f6b8cd404556888fe38e19797cbec6c3b445f4d
Hugo RICHARD <hugo.richad@inria.fr>
2022-05-01 01:09:08 +0200

    update expe

--- experiments/etcu_vs_opt.py
+++ experiments/etcu_vs_opt.py
@@ -1,9 +1,9 @@
 """Experiments."""
 
 # TODO 1, 2, FTP correct, FTP wrong
-import numpy as np
-from mlforscheduling.etc_u import opt, etc_u, flow_time
 import matplotlib.pyplot as plt
+import numpy as np
+from mlforscheduling.etc_u import etc_u, flow_time, opt
 from tqdm import tqdm
 
 n = 2000
@@ -19,7 +19,9 @@ for seed in tqdm(range(100)):
         for sc in lambda1:
             jobs1 = rng.exponential(scale=sc, size=n)
             jobs2 = rng.exponential(scale=1, size=n)
-            flow_times_alg.append(flow_time(algo(jobs1, jobs2)) / flow_time(opt(jobs1, jobs2)))
+            flow_times_alg.append(
+                flow_time(algo(jobs1, jobs2)) / flow_time(opt(jobs1, jobs2))
+            )
         flow_times.append(flow_times_alg)
     all_flow_times.append(flow_times)
 
@@ -31,12 +33,10 @@ flow_times = np.median(all_flow_times, axis=0)
 
 plt.figure()
 plt.plot(lambda1, flow_times[0], label="ETC-U")
-plt.fill_between(lambda1, flow_times_low[0],flow_times_high[0], alpha=0.3)
-plt.axvline(1/(1 + 2*np.sqrt(2)))
+plt.fill_between(lambda1, flow_times_low[0], flow_times_high[0], alpha=0.3)
+plt.axvline(1 / (1 + 2 * np.sqrt(2)))
 plt.axhline(2)
 plt.xlabel("Mean processing time of job 1 (the other job has mean 1)")
 plt.ylabel("CR")
 plt.legend()
 plt.show()
-
-
