945d658ccf4ce03e89104b2ec007662b718456b8
Flore Sentenac <floresentenac@mbp-de-flore.home>
2022-11-21 10:41:50 +0100

    uc

---
+++ mlforscheduling/ucb_rr.py
@@ -0,0 +1,58 @@
+"""Functions for scheduling based on ML predictions."""
+import numpy as np
+from mlforscheduling.utils import flow_time
+from scipy.stats import chi2
+
+def ucb_rr(jobs,delta=0.1, f=lambda n: 6 * n**2, return_type=False, return_order=False):
+    """Explore then commit with uniform exploration.
+
+    UCB in the jobs with confidence bounds of the exponential distribution
+
+    Parameters
+    ----------
+    f: int -> int
+        A function of n
+
+    jobs : np array of size k, n
+        jobs[i, j] is the processing times of the jth job of type i
+
+    Return
+    ------
+    order : np array of size kn
+        The processing times ordered as executed by the algo
+    """
+    # Assume the jobs have the same length
+    order = []
+    type_order = []
+    k, n = jobs.shape
+    m = np.zeros(k,dtype=int)
+    total_time = np.zeros(k,dtype=int)
+    bound = np.zeros(k)
+    flow_time = 0
+    while np.sum(m)<k*n:
+        current_type, current_job = select_next_job(bound,jobs,m)
+        total_time[current_type]+=1
+        if current_job-delta<0.:
+            m[current_type]+=1
+            flow_time+= (delta-current_job)*(k*n-np.sum(m))
+        else:
+            jobs[current_type,m[current_type]]-=delta
+            flow_time+= delta*(k*n-np.sum(m))
+        bound =update_confidence_bound(bound,m, current_type , total_time,n)
+    return flow_time
+            
+
+def select_next_job(bound,jobs,m):
+    current_type = np.argmax(bound)
+    current_job = jobs[current_type,m[current_type]]
+    return current_type,current_job
+
+def update_confidence_bound(bound,m, i , total_time,n):
+    if m[i]>n-1:
+        bound[i]= -float('inf')
+    else:
+        tot= np.sum(total_time)
+        mean = m[i]/total_time[i]
+        var = (m[i]*(1-mean)**2+(total_time[i]-m[i])*mean**2)/total_time[i]
+        bound[i] = mean+np.sqrt(4*var*np.log(tot)/total_time[i])+2*np.log(tot)/total_time[i]
+    return bound
---
+++ mlforscheduling/ucb_u.py
@@ -0,0 +1,57 @@
+"""Functions for scheduling based on ML predictions."""
+import numpy as np
+from mlforscheduling.utils import flow_time
+from scipy.stats import chi2
+
+def ucb_u(jobs, f=lambda n: 6 * n**2, return_type=False, return_order=False):
+    """Explore then commit with uniform exploration.
+
+    UCB in the jobs with confidence bounds of the exponential distribution
+
+    Parameters
+    ----------
+    f: int -> int
+        A function of n
+
+    jobs : np array of size k, n
+        jobs[i, j] is the processing times of the jth job of type i
+
+    Return
+    ------
+    order : np array of size kn
+        The processing times ordered as executed by the algo
+    """
+    # Assume the jobs have the same length
+    order = []
+    type_order = []
+    k, n = jobs.shape
+    m = np.zeros(k,dtype=int)
+    total_time = np.zeros(k)
+    bound = np.zeros(k)
+    for _ in range(k*n):
+        current_type, current_job = select_next_job(bound,jobs,m)
+        m[current_type]+=1
+        total_time[current_type]+= current_job
+        bound =update_confidence_bound(bound,m, current_type , total_time,n)
+        order.append(current_job)
+        type_order.append(current_type)
+    if return_order:
+        return np.array(order)
+    if return_type:
+        return np.array(type_order)
+    return flow_time(order)
+            
+
+def select_next_job(bound,jobs,m):
+    current_type = np.argmin(bound)
+    current_job = jobs[current_type,m[current_type]]
+    return current_type,current_job
+
+def update_confidence_bound(bound,m, i , total_time,n):
+    if m[i]>n-1:
+        bound[i]= float('inf')
+    else:
+        bound[i] = 2*total_time[i]/chi2.ppf(1-1/(2*n**2),2*m[i])
+    return bound
+
+
