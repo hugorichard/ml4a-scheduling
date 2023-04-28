b14556acf944dd0e963dab9781b29fec293273a9
hugorichard <research.hugo.richard@gmail.com>
2023-04-28 14:37:43 +0200

    Update docs

---
+++ doc/_templates/autosummary/function.rst
@@ -0,0 +1,8 @@
+{{ fullname | escape | underline}}
+
+.. currentmodule:: {{ module }}
+
+.. autofunction:: {{ objname }}
+
+.. _sphx_glr_backreferences_{{ fullname }}:
+
---
+++ mlforscheduling/__init__.py
@@ -0,0 +1,8 @@
+from .etc_u import etc_u
+from .etc_rr import etc_rr
+from .utils import opt, rr, ftpp
+from .lsept import lsept
+from .ucb_u import ucb_u
+from .ucb_rr import ucb_rr
+
+__version__ = '0.1'
